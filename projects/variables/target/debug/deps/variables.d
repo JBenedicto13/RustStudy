595d70b5e099732dd46988ef6ac316e8f4ebb6c5
JBenedicto13 <jbenedicto20.jbb@gmail.com>
2024-05-23 17:19:52 +0800

    3.2 Data Types

---
+++ projects/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\variables\target\debug\variables.exe: C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\variables\src\main.rs

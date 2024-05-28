a2818ec223a5b5530bdccb052ac62101b18d02b9
JBenedicto13 <jbenedicto20.jbb@gmail.com>
2024-05-28 14:41:33 +0800

    add temp_converter

---
+++ projects/temp_converter/target/debug/deps/temp_converter.d
@@ -0,0 +1,5 @@
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\temp_converter\target\debug\deps\temp_converter.exe: src\main.rs
+
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\temp_converter\target\debug\deps\temp_converter.d: src\main.rs
+
+src\main.rs:
---
+++ projects/temp_converter/target/debug/temp_converter.d
@@ -0,0 +1,1 @@
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\temp_converter\target\debug\temp_converter.exe: C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\temp_converter\src\main.rs

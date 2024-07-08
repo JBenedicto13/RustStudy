50f0abec0bf3b04934682665f5827902cc8c79ea
JBenedicto13 <jbenedicto20.jbb@gmail.com>
2024-07-08 15:20:20 +0800

    Add testing cargo

---
+++ projects/adder/target/debug/deps/adder-2faa27f9908b24bd.d
@@ -0,0 +1,7 @@
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\adder\target\debug\deps\libadder-2faa27f9908b24bd.rmeta: src\lib.rs
+
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\adder\target\debug\deps\libadder-2faa27f9908b24bd.rlib: src\lib.rs
+
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\adder\target\debug\deps\adder-2faa27f9908b24bd.d: src\lib.rs
+
+src\lib.rs:
---
+++ projects/adder/target/debug/deps/adder-b88936cc1f115a7c.d
@@ -0,0 +1,5 @@
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\adder\target\debug\deps\adder-b88936cc1f115a7c.exe: src\lib.rs
+
+C:\Users\jbenedicto\Documents\Development\Web3\Rust\projects\adder\target\debug\deps\adder-b88936cc1f115a7c.d: src\lib.rs
+
+src\lib.rs:
